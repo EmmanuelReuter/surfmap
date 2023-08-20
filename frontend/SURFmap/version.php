6dcb3f4fad62fce0b0a976ec4b622a897ca259e0
EmmanuelReuter <142700430+EmmanuelReuter@users.noreply.github.com>
2023-08-21 07:50:52 +1100

    Add files via upload
    Current version 4.0.1

--- frontend/SURFmap/version.php
+++ frontend/SURFmap/version.php
@@ -3,10 +3,13 @@
      # version.php [SURFmap]
      # Author: Rick Hofstede <r.j.hofstede@utwente.nl>
      # University of Twente, The Netherlands
+     # Contributor :
+     # Emmanuel Reuter, < emmanuel.reuter@ird.fr>
+     # French Institute for Research and Developpement
      #
      # LICENSE TERMS: 3-clause BSD license (outlined in license.html)
      *******************************/
     
-    $version = "3.3.1 (20141030)";
+    $version = "4.0.1 (20230821)";
     
 ?>
